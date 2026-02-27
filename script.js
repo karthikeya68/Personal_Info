2a835d1c56758de54f2a5b65ced20da3ec2c23ed
karthikeya68 <23mh1a4218@acoe.edu.in>
2026-02-27 22:51:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
-function openModal(cert) {
+function openModal(file) {
 
-    const modal = document.getElementById("modal");
-    const frame = document.getElementById("modalFrame");
-
-    let file = "";
-
-    if (cert === "cert1") file = "cert1.pdf";
-    if (cert === "cert2") file = "cert2.pdf";
-    if (cert === "cert3") file = "cert3.pdf";
-    if (cert === "cert4") file = "cert4.pdf";
-    if (cert === "cert5") file = "cert5.pdf";
-
-    frame.src = file;
-    modal.style.display = "block";
+    document.getElementById("modalFrame").src = file;
+    document.getElementById("modal").style.display = "flex";
 }
 
 function closeModal() {
+
     document.getElementById("modal").style.display = "none";
+    document.getElementById("modalFrame").src = "";
 }
