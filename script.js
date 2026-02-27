47e3df01a4cd3e4d5b7b7bc13f33da329e70ed77
karthikeya68 <23mh1a4218@acoe.edu.in>
2026-02-27 22:34:28 +0530

    Refactor tab and modal functions in script.js

--- script.js
+++ script.js
@@ -1,48 +1,20 @@
-function showTab(tabId) {
-    document.querySelectorAll('.tab').forEach(tab => {
-        tab.classList.remove('active');
-    });
+function openModal(cert) {
 
-    document.getElementById(tabId).classList.add('active');
+    const modal = document.getElementById("modal");
+    const frame = document.getElementById("modalFrame");
+
+    let file = "";
+
+    if (cert === "cert1") file = "cert1.pdf";
+    if (cert === "cert2") file = "cert2.pdf";
+    if (cert === "cert3") file = "cert3.pdf";
+    if (cert === "cert4") file = "cert4.pdf";
+    if (cert === "cert5") file = "cert5.pdf";
+
+    frame.src = file;
+    modal.style.display = "block";
 }
 
-function openProject(project) {
-
-    let content = "";
-
-    if (project === "p1") {
-        content = `
-        <h3>Multi-Cloud Disaster Recovery</h3>
-        <p>
-        Designed and implemented a multi-cloud disaster recovery architecture
-        using AWS and Google Cloud to ensure data availability and fault tolerance.
-        Enabled automatic backup of AWS S3 and EC2 to Google Cloud Storage and VM.
-        </p>
-        <p><b>Tools:</b> AWS, Google Cloud</p>
-        `;
-    }
-
-    if (project === "p2") {
-        content = `
-        <h3>Dog Breed Detection</h3>
-        <p>
-        Built an AI model to identify dog breeds using image recognition and
-        improved accuracy using Amazon Rekognition services.
-        </p>
-        <p><b>Tools:</b> Amazon Rekognition</p>
-        `;
-    }
-
-    if (project === "p3") {
-        content = `
-        <h3>AI Hospital Chatbot</h3>
-        <p>
-        Developed a chatbot for hospital appointment booking and symptom-based
-        suggestions with nearby hospital recommendations.
-        </p>
-        <p><b>Tools:</b> Amazon Lex</p>
-        `;
-    }
-
-    document.getElementById("projectDetails").innerHTML = content;
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
 }
