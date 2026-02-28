feef9b78f9f3094b2c8ebd899d008e088eb8d882
karthikeya68 <23mh1a4218@acoe.edu.in>
2026-02-28 10:32:06 +0530

    Refactor modal open and close functions

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function openModal(file) {
 
-    document.getElementById("modalFrame").src = file;
-    document.getElementById("modal").style.display = "flex";
+    const modal = document.getElementById("modal");
+    const frame = document.getElementById("modalFrame");
+
+    frame.src = file;
+    modal.style.display = "flex";
 }
 
 function closeModal() {
 
-    document.getElementById("modal").style.display = "none";
-    document.getElementById("modalFrame").src = "";
+    const modal = document.getElementById("modal");
+    const frame = document.getElementById("modalFrame");
+
+    modal.style.display = "none";
+    frame.src = "";
 }
